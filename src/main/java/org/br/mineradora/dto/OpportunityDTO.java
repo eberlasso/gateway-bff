110f4915be37f7fd599cb24a47686681f58b9a96
eberl <eber.slasso@gmail.com>
2025-05-25 10:43:26 -0300

    aula 37

---
+++ src/main/java/org/br/mineradora/dto/OpportunityDTO.java
@@ -0,0 +1,13 @@
+package org.br.mineradora.dto;
+
+import java.math.BigDecimal;
+
+public class OpportunityDTO {
+    private Long proposalId;
+
+    private String customer;
+
+    private BigDecimal priceTonne;
+
+    private BigDecimal lastDollarQuotation;
+}
